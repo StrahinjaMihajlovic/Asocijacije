31c54de4eb74e6730d8e69742a9185726938ab28
Strahinja Mihajlovic <strale58@hotmail.com>
2021-03-01 14:23:00 +0100

    Dodata ispravka za novije verzije MySql-a. u fajlu  /vendor/yiisoft/yii2/db/mysql/Schema.php je bilo problema gde su se nazivi vracali kao upercase sa novije baze, a u shemi su bili lowercase. Sad se podaci iz baze konvertuju u lowercase da bi se podudarali

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=Asocijacije',//podesite sledeca 3 parametra u skladu sa kredencijalima sa vaseg servera
     'username' => 'root',
     'password' => '',
-
+	'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
 
     'charset' => 'utf8',
 
